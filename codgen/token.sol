65d9b990a1c39df4eaf89a3e03b1a4c6d630294e
datumesh w <38074221+wwwarlock@users.noreply.github.com>
2019-05-23 14:04:09 +0800

    Create token.sol

---
+++ codgen/token.sol
@@ -0,0 +1,45 @@
+contract Token {
+
+    function totalSupply() constant returns (uint256 supply) {}
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {}
+
+    function transfer(address _to, uint256 _value) returns (bool success) {}
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+
+    function approve(address _spender, uint256 _value) returns (bool success) {}
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+}
+
+/*
+This implements ONLY the standard functions and NOTHING else.
+For a token like you would want to deploy in something like Mist, see HumanStandardToken.sol.
+
+If you deploy this, you won't have anything useful.
+
+Implements ERC 20 Token standard: https://github.com/ethereum/EIPs/issues/20
+
+
+.*/
+
+pragma solidity ^0.4.4;
+
+import "./Token.sol";
+
+contract StandardToken is Token {
+
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
